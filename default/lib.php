22bdf036ee01b6d2b544c4d26c9efea12fdc37cd
david <david@kaustik.com>
2013-06-16 21:58:20 +0200

    Tagit bort katalog som inte används

--- default/lib.php
+++
@@ -1,7 +0,0 @@
-<?
-
-	$db = new PDO('mysql:host=localhost;dbname=arduino', 'arduino', 'arduino');
-
-	$statement = $db->query('SELECT * FROM log ORDER BY id DESC');
-
-	$result = $statement->fetchAll(PDO::FETCH_ASSOC);
